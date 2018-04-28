9533553bc4cd30959f34d08d7cd6cdebe222a344
cc03 <croydoncodeclub@powered-up-games.com>
2018-04-28 10:29:40 +0100

    my funprogram.py

---
+++ TaeJaden/funprogram.py
@@ -0,0 +1,30 @@
+print("Hello there")
+hello=input()
+print ("What is your name")
+name=input()
+print("How are you "+ name)
+ok=input()
+if ok=="bad" or ok=="not good":
+  print ("Oh dear. That's bad")
+  iknow=input()
+  print ("Well I hope it gets sorted out")
+  print (" Try to enjoy the rest of your day "+ name)
+  thanks=input()
+  print ("Goodbye "+ name)
+  bye=input()
+else:
+  print ("That's good")
+  print (" What day is it today "+ name)
+  day=input()
+  if day=="Monday":
+   print ("I knew it!")
+   print ("That means i have to go somewhere")
+   thatsnice=input()
+   print ("see you later ,weirdo whos name i cant be bothered to remember")
+  else:
+   print ("What, I thought it was Monday")
+   print ("Are you lying o me "+ name)
+   no=input()
+   if no=="yes":
+    print("Why would you say it is "+ name)
+   
