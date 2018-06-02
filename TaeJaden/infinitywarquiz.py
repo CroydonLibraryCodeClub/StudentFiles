f07d9134fc0168d74dbe11f654fab8acb887e20b
cc06 <croydoncodeclub@powered-up-games.com>
2018-06-02 10:38:35 +0100

    more quiz

---
+++ TaeJaden/infinitywarquiz.py
@@ -0,0 +1,124 @@
+print("\n") # Blank lines ("\n" means "new line")
+print("Welcome to the ultimate immpossible Avengers Infinity war quiz!!!")
+score = 0
+print("\n") # Blank lines ("\n" means "new line")
+print(" Qustion 1. Who is the main villian of Infinity war?")
+print("A. Green Goblin")
+print("B. Thanos")
+print("C. Doctor Octopus")
+print("D. Spider man")
+choice = input("Type in a letter to guess: ")
+
+if choice == "B" or choice == "b":
+    print("Well done, that's right!")
+    score = score + 1
+else:
+    print("Wrong, wrong, wrong!")
+
+print("\n") # Blank lines ("\n" means "new line")
+
+print("Question 2. Who is the first to die?")
+print("A. Loki")
+print("B. Gamora")
+print("C. Nebula")
+print("D. Thor")
+choice = input("Type in a letter to guess: ")
+
+if choice == "A" or choice == "a":
+    print("Well done, that's right!")
+    score = score + 1
+else:
+    print("Wrong, wrong, wrong!")
+
+print("\n") # Blank lines ("\n" means "new line")
+print("Question 3. How many infinity stones are there?")
+print("A. 11")
+print("B.  1")
+print("C. 6")
+print("D. 2")
+choice = input("Type in a letter to guess: ")
+
+if choice == "C" or choice == "c":
+    print("Well done, that's right!")
+    score = score + 1
+else:
+    print("Wrong, wrong, wrong!")
+
+print(".Question 4 Who nearly killed Thanos?")
+print("A. Iron man")
+print("B. Spider man")
+print("C. Thor")
+print("D. Gamora")
+choice = input("Type in a letter to guess: ")
+
+if choice == "C" or choice == "c":
+    print("Well done, that's right!")
+    score = score + 1
+else:
+    print("Wrong, wrong, wrong!")
+
+print("\n")
+
+print("The end of the infinity war quiz! You scored " + str(score))
+print("\n")
+print("Nice. Now give this next quiz... Marvel knowledge!")
+print("\n")
+score = 0
+print(".Question 1. What is Star-lord's really name?")
+print("A. Tony Stark ")
+print("B. Peter Parker")
+print("C. Thor")
+print("D. Peter Quill")
+choice = input("Type in a letter to guess: ")
+
+if choice == "D" or choice == "d":
+    print("Well done, that's right!")
+    score = score + 1
+else:
+    print("Wrong, wrong, wrong!")
+
+print("\n")
+print(".Question 2. What is Thanos?")
+print("A. A God ")
+print("B. A Celestial")
+print("C. A Titan")
+print("D. A King")
+choice = input("Type in a letter to guess: ")
+
+if choice == "C" or choice == "c":
+    print("Well done, that's right!")
+    score = score + 1
+else:
+    print("Wrong, wrong, wrong!")
+
+print("\n")
+print(".Question 3. Who is N'Jadaka known as?")
+print("A. Erik Stevens")
+print("B. Thor")
+print("C. T'Challa")
+print("D. Peter Quill")
+choice = input("Type in a letter to guess: ")
+
+if choice == "A" or choice == "a":
+    print("Well done, that's right!")
+    score = score + 1
+else:
+    print("Wrong, wrong, wrong!")
+
+print("\n")
+print(".Question 4. Who is the winter solider?")
+print("A. Erik Stevens")
+print("B. Bucky Barnes")
+print("C. Tony Stark")
+print("D. Peter Parker")
+choice = input("Type in a letter to guess: ")
+
+if choice == "B" or choice == "b":
+    print("Well done, that's right!")
+    score = score + 1
+else:
+    print("Wrong, wrong, wrong!")
+print("\n")
+print("The end of the infinity war quiz! You scored " + str(score))
+input("\nPress ENTER to exit program")
+
