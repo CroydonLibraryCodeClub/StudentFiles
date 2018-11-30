bc839b850346ac763cc9f28eb93a2b6cae1e0472
cc03 <croydoncodeclub@powered-up-games.com>
2018-11-30 22:58:15 +0000

    update

---
+++ Matin/Cubicalistic/Assets/HitBlock.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitBlock : MonoBehaviour {
+
+	Vector3 position;
+
+	void Start()
+	{
+		position = transform.position;
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		transform.position = position;
+	}
+}
