836bde5baf651c6d4290d4d7d7000904e07f1614
cc01 <cc01@powered-up-games.com>
2022-04-30 11:00:48 +0100

    Kevin

---
+++ Kevin/pythkev.py
@@ -0,0 +1,13 @@
+from datetime import *
+print(datetime.now())
+python = '🐍⌨️'#
+sums = '✖️➗➖➕'
+hundred = '💯'
+print("Hello, world")
+print("Hello, I'm Python!")
+print("I'm joking my real name is not Kevin")
+print("I dont really know python")
+print("🌍🌍🌍,snake⌨️")
+print(python, 'is very good at', sums)
+print((2 + 4) * (5 + 3))
+print((41*2) +(54/7))
