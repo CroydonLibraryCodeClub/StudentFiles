ff4a02e62ca802839de2ff3cce8d96415931dc91
cc10 <cc10@powered-up-games.com>
2023-03-25 06:15:48 -0400

    max

--- Max/Mazescape/Assets/PlayerMovement.cs
+++ Max/Mazescape/Assets/PlayerMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public float delay = 1f;
+    float timer;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,29 +16,34 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = rb.position;
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        timer += Time.deltaTime;
+        if (timer > delay)
         {
-            position.y += 1;
-        }
-        rb.MovePosition(position);
+            Vector2 position = rb.position;
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                position.y += 1;
+            }
+            rb.MovePosition(position);
 
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            position.y += -1;
-        }
-        rb.MovePosition(position);
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                position.y += -1;
+            }
+            rb.MovePosition(position);
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            position.x += -1;
-        }
-        rb.MovePosition(position);
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                position.x += -1;
+            }
+            rb.MovePosition(position);
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            position.x += 1;
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                position.x += 1;
+            }
+            rb.MovePosition(position);
+            timer = 0;
         }
-        rb.MovePosition(position);
     }
 }
