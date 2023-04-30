c409a4e81aa93c753c0f72422f4b3a96e167a822
Paul Sinnett <paul.sinnett@gmail.com>
2023-04-30 10:31:23 +0100

    tweaks
    some adjustments were needed to make it work as intended across platforms and devices

--- Max/Delivery/Assets/PlayerMovement.cs
+++ Max/Delivery/Assets/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D physics;
     public float force = 1;
     public float speed = 1;
+    Vector2 direction;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +17,12 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    void FixedUpdate()
+    {
         Vector2 speedWanted = direction * speed;
-        physics.AddForce((speedWanted - physics.velocity)*force);
+        physics.AddForce((speedWanted - physics.velocity) * force);
     }
 }
