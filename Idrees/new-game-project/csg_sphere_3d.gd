3b2766a17aab61c598d11854ff0aff8803de23b5
cc11 <cc11@powered-up-games.com>
2026-05-30 10:59:21 +0100

    basketball

--- Idrees/new-game-project/csg_sphere_3d.gd
+++ Idrees/new-game-project/csg_sphere_3d.gd
@@ -1,8 +1,8 @@
 extends RigidBody3D
 func _process(_delta):
 	if Input.is_action_pressed("ui_right"):
-		linear_velocity.x = 1
+		apply_force(Vector3(1, 0, 0))
 	if Input.is_action_pressed('ui_left'):
-		linear_velocity.x = -1
+		apply_force(Vector3(-1, 0, 0))
 	if Input.is_action_just_pressed('ui_up'):
 		linear_velocity.y = 5
