449418cd5316ebfdb465d391a75dd378c716bcc1
cc07 <cc07@powered-up-games.com>
2026-06-13 11:01:51 +0100

    forwards and backwards

--- Idrees/new-game-project/csg_sphere_3d.gd
+++ Idrees/new-game-project/csg_sphere_3d.gd
@@ -4,5 +4,11 @@ func _process(_delta):
 		apply_force(Vector3(1, 0, 0))
 	if Input.is_action_pressed('ui_left'):
 		apply_force(Vector3(-1, 0, 0))
-	if Input.is_action_just_pressed('ui_up'):
+	if Input.is_action_just_pressed('ui_accept'):
+		apply_force(Vector3(0, 1, 0))
 		linear_velocity.y = 5
+	if Input.is_action_pressed("ui_up"):
+		apply_force(Vector3(0, 0,-1))
+	if Input.is_action_pressed("ui_down"):
+		apply_force(Vector3(0, 0,1))
+		
